f8ae2ded2094b86109f7f07eab80600b6db9e078
richer <yxxchange@163.com>
2026-03-19 16:37:42 +0800

    refactor: split db init and migrate responsibilities

--- infra/mysql/model/base.go
+++ infra/mysql/model/base.go
@@ -1,9 +1,14 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type BaseModel struct {
-	ID        int64     `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
-	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime" json:"created_at"`
-	UpdatedAt time.Time `gorm:"column:updated_at;autoUpdateTime" json:"updated_at"`
+	ID        int64          `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
+	CreatedAt time.Time      `gorm:"column:created_at;autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time      `gorm:"column:updated_at;autoUpdateTime" json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at;index" json:"deleted_at,omitempty"`
 }
